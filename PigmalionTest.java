a571c2f6bd5e85812e58a5525d6a76f7372dee86
Franco Lens <francolens2002@gmail.com>
2025-01-02 19:37:40 -0300

    Modificando valor del input

--- PigmalionTest.java
+++ PigmalionTest.java
@@ -3,18 +3,28 @@ import java.util.List;
 
 public class PigmalionTest {
 
+    /*
+    * Función 1, que asume que los recursos son infinitos y no le da importancia al tiempo
+    */
+    public static boolean encontrarSuma(int[] nums, int sumaRequerida) {
+        //Imprime el array otorgado en cuestion
+        System.out.println(nums);
 
-    public static boolean encontrarSuma(List<Integer> arr, int sumaRequerida) {
+        /* Itera sobre el array 2 veces
+        * La primera para obtener el primer valor y la segunda para sumarlo contra los demas valores
+        * */
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
 
-        System.out.println(arr);
+                /*  Si i y j no son iguales y la suma de ambos es igual a la suma requerida imprime true
+               *  Esto es para evitar que se sume el mismo indice
+               */
 
-        for (int i = 0; i < arr.size(); i++) {
-            for (int j = 0; j < arr.size(); j++) {
-                if (i != j && arr.get(i) + arr.get(j) == sumaRequerida) {
+                if (i != j && nums[i] + nums[j] == sumaRequerida) {
                     return true;
                 }
             }
-
+        //En caso de que no coincida con la suma devuelve false
         }
         return false;
     }
@@ -22,8 +32,8 @@ public class PigmalionTest {
 
 
     public static void main(String[] Args){
-        List<Integer> unsoArray = Arrays.asList(1, 4, 3, 9);
-        System.out.println(encontrarSuma(unsoArray,8));
+        int[] x = {1, 4, 3, 9};
+        System.out.println(encontrarSuma(x,8));
 
     }
 }
